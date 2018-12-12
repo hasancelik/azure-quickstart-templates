ffec3e30b89d5209021b0eb251dbbbf239f17981
hasancelik <hasan@hazelcast.com>
2018-12-12 14:55:32 +0300

    v3

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -1,13 +1,15 @@
-$CLUSTER_NAME=$args[0]
-$CLUSTER_PASSWORD=$args[1]
-$SUBSCRIPTION_ID=$args[2]
-$AAD_CLIENT_ID=$args[3]
-$AAD_CLIENT_SECRET=$args[4]
-$AAD_TENANT_ID=$args[5]
-$GROUP_NAME=$args[6]
-$CLUSTER_TAG=$args[7]
+param (
+    [string]$clusterName,
+    [string]$clusterPassword,
+    [string]$subscriptionId,
+    [string]$aadClientId,
+    [string]$aadClientSecret,
+    [string]$aadTenantId,
+    [string]$groupName,
+    [string]$clusterTag
+)
 
-python .\modify_configuration --cluster-name=$CLUSTER_NAME --cluster-password=$CLUSTER_PASSWORD --subscription-id=$SUBSCRIPTION_ID --aad-client-id=$AAD_CLIENT_ID --aad-client-secret=$AAD_CLIENT_SECRET --tenant-id=$AAD_TENANT_ID --group-name=$GROUP_NAME --cluster-tag=$CLUSTER_TAG --filename=hazelcast.xml
+python .\modify_configuration --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
 & .\install_hazelcast.ps1
 
 mvn exec:java -f C:\Temp\hazelcast\pom.xml
