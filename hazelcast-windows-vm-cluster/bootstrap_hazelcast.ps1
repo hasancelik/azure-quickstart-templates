91c64b7fc40d9de0df5a14a88aa15ffc1db4d5ed
hasancelik <hasan@hazelcast.com>
2018-12-14 15:02:02 +0300

    add py file

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -9,7 +9,7 @@ param (
     [string]$clusterTag
 )
 
-python .\modify_configuration --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
+python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
 & .\install_hazelcast.ps1
 
 mvn exec:java -f C:\Temp\hazelcast\pom.xml
