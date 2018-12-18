deb91c7fa94ad589e5ddfcb7be7dfefe605029be
hasancelik <hasan@hazelcast.com>
2018-12-18 17:46:50 +0300

    updated bootstrap

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -10,6 +10,6 @@ param (
 )
 
 python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
-& .\install_hazelcast.ps1
+.\install_hazelcast.ps1
 
 mvn exec:java -f C:\Temp\hazelcast\pom.xml
