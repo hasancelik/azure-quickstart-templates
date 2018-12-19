303a83ebd6f5320b6531f4f24894ee2b7281d175
hasancelik <hasan@hazelcast.com>
2018-12-19 13:26:14 +0300

    refresh

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -10,8 +10,10 @@ param (
 )
 
 .\install_hazelcast.ps1
+refreshenv
+
 python -V
-python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml | Out-File -FilePath C:\modify.log
+python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=.\hazelcast.xml | Out-File -FilePath C:\modify.log
 
 Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
 
