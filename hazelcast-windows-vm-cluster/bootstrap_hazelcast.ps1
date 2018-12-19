074fe7697fe92e60762a2c9252a73120f1047c49
hasancelik <hasan@hazelcast.com>
2018-12-19 12:09:39 +0300

    added outputs

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -9,8 +9,15 @@ param (
     [string]$clusterTag
 )
 
+Write-Host "Installing tools and copying files"
 .\install_hazelcast.ps1
-python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
-Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
+Write-Host "Finished"
+Write-Host "Run python file"
+python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml > C:\modify.log
+Write-Host "Finished"
 
-mvn exec:java -f C:\Temp\hazelcast\pom.xml
+Write-Host "Copying hazelcast.xml"
+Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
+Write-Host "Finished"
+
+mvn exec:java -f C:\Temp\hazelcast\pom.xml > C:\maven.log
