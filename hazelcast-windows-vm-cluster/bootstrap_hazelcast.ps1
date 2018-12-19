7ff5aed633e7aedfb6b4d830e0b5cfcdfa86e13b
hasancelik <hasan@hazelcast.com>
2018-12-19 11:32:50 +0300

    changed script order

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -9,7 +9,8 @@ param (
     [string]$clusterTag
 )
 
-python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
 .\install_hazelcast.ps1
+python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=hazelcast.xml
+Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
 
 mvn exec:java -f C:\Temp\hazelcast\pom.xml
--- hazelcast-windows-vm-cluster/install_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/install_hazelcast.ps1
@@ -11,9 +11,7 @@ choco install python -y
 New-Item -ItemType directory -Force -Path C:\Temp\hazelcast
 
 # copy hazelcast.xml and pom.xml into C:\Temp\hazelcast
-
 Copy-Item .\pom.xml -Destination C:\Temp\hazelcast
-Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
 
 Copy-Item .\bootstrap_hazelcast.ps1 -Destination C:\Temp\hazelcast
 Copy-Item .\modify_configuration.py -Destination C:\Temp\hazelcast
