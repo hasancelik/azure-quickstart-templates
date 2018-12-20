dc007b021f73a1a36ea30a40553c9bd9b8b66600
hasancelik <hasan@hazelcast.com>
2018-12-20 10:54:56 +0300

    added python exe

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -9,11 +9,13 @@ param (
     [string]$clusterTag
 )
 
+$python = "C:\Python37\python.exe"
+
 .\install_hazelcast.ps1
 refreshenv
 
-python -V
-python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=.\hazelcast.xml | Out-File -FilePath C:\modify.log
+$python -V
+$python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=.\hazelcast.xml | Out-File -FilePath C:\modify.log
 
 Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
 
