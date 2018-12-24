0da5f344cec1f23b6c866a9008ae356a7fbcd260
hasancelik <hasan@hazelcast.com>
2018-12-24 17:24:39 +0300

    specific tool versions

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -13,23 +13,15 @@ param (
 refreshenv
 
 # $python = 'C:\Python37\python.exe'
-# & $python -V
-
 # $java = 'C:\Program Files\Java\jdk1.8.0_191\bin\java.exe'
 # $mvn = 'C:\ProgramData\chocolatey\bin\mvn.exe'
+# & $python -V
 # & $java -version
 # & $mvn --version
 
 python -V
 java -version
 mvn --version
-
-Get-Command java | Select-Object Version
-Get-Command java | Select-Object Source
-Get-Command mvn | Select-Object Version
-Get-Command mvn | Select-Object Source
-Get-Command python | Select-Object Version
-Get-Command python | Select-Object Source
 
 python .\modify_configuration.py --cluster-name=$clusterName --cluster-password=$clusterPassword --subscription-id=$subscriptionId --aad-client-id=$aadClientId --aad-client-secret=$aadClientSecret --tenant-id=$aadTenantId --group-name=$groupName --cluster-tag=$clusterTag --filename=.\hazelcast.xml | Out-File -FilePath C:\modify.log
 Copy-Item .\hazelcast.xml -Destination C:\Temp\hazelcast
--- hazelcast-windows-vm-cluster/install_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/install_hazelcast.ps1
@@ -5,8 +5,8 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install jdk8 -y
-choco install maven -y
-choco install python -y
+choco install maven --version 3.5.4 -y
+choco install python3 --version 3.7.0 -y
 
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
