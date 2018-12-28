b572aaeca7f1cf7eca2787e39c3472670d07b839
hasancelik <hasan@hazelcast.com>
2018-12-28 11:48:05 +0300

    fixed job

--- hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/bootstrap_hazelcast.ps1
@@ -16,15 +16,4 @@ refreshenv
 # Openning the port of hazelcast member
 netsh advfirewall firewall add rule name="Hazelcast Member" dir=in action=allow protocol=TCP localport=$clusterPort
 
-$mvnbuild = "C:\ProgramData\chocolatey\bin\mvn.exe"
-$mvnargs = "exec:java --file C:\Temp\hazelcast\pom.xml"
-$hazelcasttemp = "C:\Temp\hazelcast"
-
-# maven dowloads are very slow with Start-Process so download them before execution.
-mvn clean install --file "C:\Temp\hazelcast\pom.xml"
-Write-Out "Maven process is starting...."
-Start-Process -FilePath $mvnbuild -ArgumentList $mvnargs  -RedirectStandardOutput "$hazelcasttemp\hazelcast-standard.txt" -RedirectStandardError "$hazelcasttemp\hazelcast-member.txt" -PassThru
-Write-Out "Process finished"
-Write-Out "Job is starting"
-Start-Job -Name "Hazelcast" -ScriptBlock {Get-Process mvn}
-Write-Out "Jib finished"
+Start-Job -ScriptBlock {Set-Location "C:\Temp\hazelcast"; mvn exec:java 2>&1 >> "C:\Temp\hazelcast\hazelcast-member.log"}
