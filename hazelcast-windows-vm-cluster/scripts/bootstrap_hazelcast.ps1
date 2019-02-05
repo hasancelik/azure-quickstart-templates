05feb36eeda1105572fc7082e138f6b3d1c60ea2
hasancelik <hasan@hazelcast.com>
2019-02-05 18:16:43 +0300

    add sleep

--- hazelcast-windows-vm-cluster/scripts/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/scripts/bootstrap_hazelcast.ps1
@@ -18,6 +18,8 @@ netsh advfirewall firewall add rule name="Hazelcast Member" dir=in action=allow 
 
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy Unrestricted -Command "cd C:\Temp\hazelcast; mvn exec:java 2>&1 >> "C:\Temp\hazelcast\hazelcast-member.log""'
 $trigger =  New-ScheduledTaskTrigger -AtStartup
-
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Hazelcast" -Description "Hazelcast Member"
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Hazelcast" -Description "Hazelcast Member" -RunLevel Highest
+Get-ScheduledTask -TaskName "Hazelcast"
 Start-ScheduledTask -TaskName "Hazelcast"
+Start-Sleep -Seconds 300
+Get-ScheduledTask -TaskName "Hazelcast"
