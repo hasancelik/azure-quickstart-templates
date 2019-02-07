5026bc4bc3100085612c4e9e9bd0354f6680efda
hasancelik <hasan@hazelcast.com>
2019-02-07 16:50:59 +0300

    removed echos and sleep

--- hazelcast-windows-vm-cluster/scripts/bootstrap_hazelcast.ps1
+++ hazelcast-windows-vm-cluster/scripts/bootstrap_hazelcast.ps1
@@ -18,14 +18,8 @@ refreshenv
 # Openning the port of hazelcast member
 netsh advfirewall firewall add rule name="Hazelcast Member" dir=in action=allow protocol=TCP localport=$clusterPort
 
-echo $user
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy Unrestricted -Command "cd C:\Temp\hazelcast; mvn exec:java 2>&1 >> "C:\Temp\hazelcast\hazelcast-member.log""'
-echo '1'
-$trigger =  New-ScheduledTaskTrigger -AtStartup
-echo '2'
+$trigger = New-ScheduledTaskTrigger -AtStartup
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Hazelcast" -User $user -Password $userPassword -Description "Hazelcast Member" -RunLevel Highest
-echo '3'
 Get-ScheduledTask -TaskName "Hazelcast"
 Start-ScheduledTask -TaskName "Hazelcast"
-Start-Sleep -Seconds 300
-Get-ScheduledTask -TaskName "Hazelcast"
